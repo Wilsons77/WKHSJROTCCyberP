2a9fc2fbb3b51f37d0cc90c416a33fe16d13d99e
Wilsons77 <shaunp6907@gmail.com>
2022-10-21 23:16:13 -0400

    Update Windows.bat

--- Windows.bat
+++ Windows.bat
@@ -1,191 +1,383 @@
-echo Disabling Guest account...
-net user Guest /active:no >nul
-
-if ERRORLEVEL 1 (
-  echo Disabling Guest Account failed.
-  timeout 30 >nul
-  exit
+@echo off
+setlocal enabledelayedexpansion
+net session
+if %errorlevel%==0 (
+	echo You are running this as Administrator
+) else (
+    echo Failure, Please run this as Administrator
+	pause
+    exit
 )
 
-echo Guest account disabled
-
-
-REM ~~~~~~~~~~~~~~~~~
-
-echo Disabling Admin account...
-net user Administrator /active:no >nul
-
-if ERRORLEVEL 1 (
-  echo Disabling Admin Account failed.
-  timeout 30 >nul
-  exit
-)
-
-echo Admin account disabled
-
-REM ~~~~~~~~~~~~~~~~~
-
-echo Setting MAXPWAGE to 14 days...
-net accounts /maxpwage:14 >nul
-
-if ERRORLEVEL 1 (
-  echo An error occured while setting MAXPWAGE.
-  timeout 30 >nul
-  exit
-)
-
-echo Maximum password life set.
-
-
-REM ~~~~~~~~~~~~~~~~~
-
-echo Setting MINPWLENGTH to 10 characters...
-net accounts /minpwlen:10 >nul
-
-if ERRORLEVEL 1 (
-  echo An error occured while setting MINPWLENGTH.
-  timeout 30 >nul
-  exit
-)
-
-echo Minimum password length set.
-
-
-REM ~~~~~~~~~~~~~~~~~
-
-echo Setting lockout duration to 45 minutes...
-net accounts /lockoutduration:45 >nul
-
-if ERRORLEVEL 1 (
-  echo An error occured while setting lockout duration.
-  timeout 30 >nul
-  exit
-)
-
-echo Lockout duration policy is enforced.
-
-
-
-REM ~~~~~~~~~~~~~~~~~
-
-echo Setting lockout threshold to 3 attempts...
-net accounts /lockoutthreshold:3 >nul
-
-if ERRORLEVEL 1 (
-  echo An error occured while setting lockout threshold.
-  timeout 30 >nul
-  exit
-)
-
-echo Lockout threshold enforced.
-
-
-
-
-REM ~~~~~~~~~~~~~~~~~
-
-
-echo Setting lockout window to 15 minutes...
-net accounts /lockoutwindow:15 >nul
-
-if ERRORLEVEL 1 (
-  echo An error occured while setting lockout window.
-  timeout 30 >nul
-  exit
-)
-
-echo Lockout window enforced.
-
-
-
-REM ~~~~~~~~~~~~~~~~~
-
-echo Begin auditing successful and unsuccessful logon/logoff attempts...
-auditpol /set /category:"Account Logon" /Success:enable /failure:enable >nul
-auditpol /set /category:"Logon/Logoff" /Success:enable /failure:enable >nul
-auditpol /set /category:"Account Management" /Success:enable /failure:enable >nul
-auditpol /set /category:"DS Access" /failure:enable >nul
-auditpol /set /category:"Object Access" /failure:enable >nul
-auditpol /set /category:"policy change" /Success:enable /failure:enable >nul
-auditpol /set /category:"Privilege use" /Success:enable /failure:enable >nul
-auditpol /set /category:"System" /failure:enable >nul
-
-if ERRORLEVEL 1 (
-  echo An error occured while enabling logging for logon and logoff attempts.
-  timeout 30 >nul
-  exit
-)
-
-echo Now logging all logon and logoff attempts
-
-
-
-
-REM ~~~~~~~~~~~~~~~~~
-
-echo Attempting to block FTP (20, 21)...
-netsh advfirewall firewall add rule name="BlockFTP20" protocol=TCP dir=in localport=20 action=block >nul
-netsh advfirewall firewall add rule name="BlockFTP21" protocol=TCP dir=in localport=21 action=block >nul
-
-if ERRORLEVEL 1 (
-  echo An error occured while blocking FTP.
-  timeout 30 >nul
-  exit
-)
-
-echo FTP is blocked.
-
-
-
-
-REM ~~~~~~~~~~~~~~~~~
-
-echo Attempting to block TCP/Telnet (23)...
-netsh advfirewall firewall add rule name="BlockTelNet23" protocol=TCP dir=in localport=23 action=block >nul
-
-if ERRORLEVEL 1 (
-  echo An error occured while blocking TelNet.
-  timeout 30 >nul
-  exit
-)
-
-echo TelNet is blocked.
-
-
-
-
-REM ~~~~~~~~~~~~~~~~~
-
-
-echo Attempting to deny RDP access...
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 1 /f >nul
-
-if ERRORLEVEL 1 (
-  echo An error occured while denying RDP.
-  timeout 30 >nul
-  exit
-)
-
-echo RDP connections are now being denied.
-
-
-
-REM ~~~~~~~~~~~~~~~~~
-
-
-echo Attempting to enable Windows Firewall...
-NetSh Advfirewall set allprofiles state on
-Netsh Advfirewall show allprofiles
-
-if ERRORLEVEL 1 (
-  echo Error enabling Windows Firewall.
-  timeout 30 >nul
-  exit
-)
-
-echo Windows Firewall enabled.
-
-
-REM netstat -aon | find /i "listening"
-
-REM (wmic service get  name, startname /format:htable >out.html) && out.html
+cls
+
+set /p answer=Have you answered all the forensics questions?[y/n]: 
+	if /I {%answer%}=={y} (
+		goto :menu
+	) else (
+		echo Complete these first before looking for issues.
+		pause
+		exit
+	)
+	
+:menu
+	cls
+	echo "##############################################################"
+	echo "1)Set user properties		10)Disable services"
+	echo "2)Create a user		        11)Turn on UAC"
+	echo "3)Disable a user			12)remote Desktop Config"
+	echo "4)Change all passwords		13)Enable auto update"
+	echo "5)Disable guest/admin	        14)Security options"
+	echo "6)Set password policy		15)Audit the machine	
+	echo "7)Set lockout policy		16)Edit groups"
+	echo "8)Enable Firewall"
+	echo "9)Search for media files"
+	echo "20)Quit			25)Restart"
+	echo "##############################################################"
+	set /p answer=Please choose an option: 
+		if "%answer%"=="1" goto :userProp
+		if "%answer%"=="2" goto :createUser
+		if "%answer%"=="3" goto :disUser
+		if "%answer%"=="4" goto :passwd
+		if "%answer%"=="5" goto :disGueAdm
+		if "%answer%"=="6" goto :passwdPol
+		if "%answer%"=="7" goto :lockout
+		if "%answer%"=="8" goto :firewall
+		if "%answer%"=="9" goto :badFiles
+		if "%answer%"=="10" goto :services
+		if "%answer%"=="11" goto :UAC
+		if "%answer%"=="12" goto :remDesk
+		if "%answer%"=="13" goto :autoUpdate
+		if "%answer%"=="14" goto :secOpt
+		if "%answer%"=="15" goto :audit
+		if "%answer%"=="16" goto :group
+		if "%answer%"=="20" exit
+		if "%answer%"=="25" shutdown /r
+	pause
+
+:userProp
+	echo Setting password never expires
+	wmic UserAccount set PasswordExpires=True
+	wmic UserAccount set PasswordChangeable=True
+	wmic UserAccount set PasswordRequired=True
+
+	pause
+	goto :menu
+
+:passwd
+	echo Changing all user passwords
+
+	endlocal
+	setlocal EnableExtensions
+	for /F "tokens=2* delims==" %%G in ('
+		wmic UserAccount where "status='ok'" get name >null
+	') do for %%g in (%%~G) do (
+		net user %%~g Cyb3rPatr!0t$
+		)
+	endlocal
+	setlocal enabledelayedexpansion	
+	pause
+	goto :menu
+
+:disUser
+	cls
+	net users
+	set /p answer=Would you like to delete a user?[y/n]: 
+	if /I "%answer%"=="y" (
+		cls
+		net users
+		set /p DISABLE=What is the name of the user?:
+			net user !DISABLE! /active:no
+		echo !DISABLE! has been disabled
+		pause
+		goto :disUser
+	)
+
+	pause
+	goto :menu
+
+:createUser
+	set /p answer=Would you like to create a user?[y/n]: 
+	if /I "%answer%"=="y" (
+		set /p NAME=What is the user you would like to create?:
+		net user !NAME! /add
+		echo !NAME! has been added
+		pause 
+		goto :createUser
+	) 
+	if /I "%answer%"=="n" (
+		goto :menu
+	)
+
+:disGueAdm
+	rem Disables the guest account
+	net user Guest | findstr Active | findstr Yes
+	if %errorlevel%==0 (
+		echo Guest account is already disabled.
+	)
+	if %errorlevel%==1 (
+		net user guest Cyb3rPatr!0t$ /active:no
+	)
+
+	rem Disables the Admin account
+	net user Administrator | findstr Active | findstr Yes
+	if %errorlevel%==0 (
+		echo Admin account is already disabled.
+		pause
+		goto :menu
+	)
+	if %errorlevel%==1 (
+		net user administrator Cyb3rPatr!0t$ /active:no
+		pause
+		goto :menu
+	)
+
+:passwdPol
+	rem Sets the password policy
+	rem Set complexity requirments
+	echo Setting pasword policies
+	net accounts /minpwlen:8
+	net accounts /maxpwage:60
+	net accounts /minpwage:10
+	net accounts /uniquepw:3
+
+	pause
+	goto :menu
+
+:lockout
+	rem Sets the lockout policy
+	echo Setting the lockout policy
+	net accounts /lockoutduration:30
+	net accounts /lockoutthreshold:3
+	net accounts /lockoutwindow:30
+
+	pause
+	goto :menu
+
+:firewall
+	rem Enables firewall
+	netsh advfirewall set allprofiles state on
+	netsh advfirewall reset
+
+	pause
+	goto :menu
+
+:badFiles
+	goto :menu
+
+:services
+	echo Disabling Services
+	sc stop TapiSrv
+	sc config TapiSrv start= disabled
+	sc stop TlntSvr
+	sc config TlntSvr start= disabled
+	sc stop ftpsvc
+	sc config ftpsvc start= disabled
+	sc stop SNMP
+	sc config SNMP start= disabled
+	sc stop SessionEnv
+	sc config SessionEnv start= disabled
+	sc stop TermService
+	sc config TermService start= disabled
+	sc stop UmRdpService
+	sc config UmRdpService start= disabled
+	sc stop SharedAccess
+	sc config SharedAccess start= disabled
+	sc stop remoteRegistry 
+	sc config remoteRegistry start= disabled
+	sc stop SSDPSRV
+	sc config SSDPSRV start= disabled
+	sc stop W3SVC
+	sc config W3SVC start= disabled
+	sc stop SNMPTRAP
+	sc config SNMPTRAP start= disabled
+	sc stop remoteAccess
+	sc config remoteAccess start= disabled
+	sc stop RpcSs
+	sc config RpcSs start= disabled
+	sc stop HomeGroupProvider
+	sc config HomeGroupProvider start= disabled
+	sc stop HomeGroupListener
+	sc config HomeGroupListener start= disabled
+
+	pause
+	goto :menu
+:UAC
+
+	rem Enable UAC
+	echo Turning on UAC has to be done manually.
+
+	pause
+	goto :menu
+
+:remDesk
+	rem Ask for remote desktop
+	set /p answer=Do you want remote desktop enabled?[y/n]
+	if /I "%answer%"=="y" (
+		reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+		echo RemoteDesktop has been enabled, reboot for this to take full effect.
+	)
+	if /I "%answer%"=="n" (
+		reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 1 /f
+		echo RemoteDesktop has been disabled, reboot for this to take full effect.
+	)
+
+	pause
+	goto :menu
+
+:autoUpdate
+	rem Turn on automatic updates
+	echo Turning on automatic updates
+	reg add "HKLM\SOFTWARE\Microsoft\WINDOWS\CurrentVersion\WindowsUpdate\Auto Update" /v AUOptions /t REG_DWORD /d 4 /f
+
+	pause
+	goto :menu
+
+:secOpt
+	echo Changing security options now.
+
+	rem Restrict CD ROM drive
+	reg ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AllocateCDRoms /t REG_DWORD /d 1 /f
+
+	rem Automatic Admin logon
+	reg ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /t REG_DWORD /d 0 /f
+
+	rem Logon message text
+	set /p body=NO UNAUTHORISED ACCESS: 
+		reg ADD "HKLM\SYSTEM\microsoft\Windwos\CurrentVersion\Policies\System\legalnoticetext" /v LegalNoticeText /t REG_SZ /d "%body%"
+
+	rem Logon message title bar
+	set /p subject=WKHS NJROTC: 
+		reg ADD "HKLM\SYSTEM\microsoft\Windwos\CurrentVersion\Policies\System\legalnoticecaption" /v LegalNoticeCaption /t REG_SZ /d "%subject%"
+
+	rem Wipe page file from shutdown
+	reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v ClearPageFileAtShutdown /t REG_DWORD /d 1 /f
+
+	rem Disallow remote access to floppie disks
+	reg ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AllocateFloppies /t REG_DWORD /d 1 /f
+
+	rem Prevent print driver installs 
+	reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Print\Providers\LanMan Print Services\Servers" /v AddPrinterDrivers /t REG_DWORD /d 1 /f
+
+	rem Limit local account use of blank passwords to console
+	reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v LimitBlankPasswordUse /t REG_DWORD /d 1 /f
+
+	rem Auditing access of Global System Objects
+	reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v auditbaseobjects /t REG_DWORD /d 1 /f
+
+	rem Auditing Backup and Restore
+	reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v fullprivilegeauditing /t REG_DWORD /d 1 /f
+
+	rem Do not display last user on logon
+	reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v dontdisplaylastusername /t REG_DWORD /d 1 /f
+
+	rem UAC setting (Prompt on Secure Desktop)
+	reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v PromptOnSecureDesktop /t REG_DWORD /d 1 /f
+
+	rem Enable Installer Detection
+	reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableInstallerDetection /t REG_DWORD /d 1 /f
+
+	rem Undock without logon
+	reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v undockwithoutlogon /t REG_DWORD /d 0 /f
+
+	rem Maximum Machine Password Age
+	reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v MaximumPasswordAge /t REG_DWORD /d 15 /f
+
+	rem Disable machine account password changes
+	reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v DisablePasswordChange /t REG_DWORD /d 1 /f
+
+	rem Require Strong Session Key
+	reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v RequireStrongKey /t REG_DWORD /d 1 /f
+
+	rem Require Sign/Seal
+	reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v RequireSignOrSeal /t REG_DWORD /d 1 /f
+
+	rem Sign Channel
+	reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v SignSecureChannel /t REG_DWORD /d 1 /f
+
+	rem Seal Channel
+	reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v SealSecureChannel /t REG_DWORD /d 1 /f
+
+	rem Don't disable CTRL+ALT+DEL even though it serves no purpose
+	reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v DisableCAD /t REG_DWORD /d 0 /f 
+
+	rem Restrict Anonymous Enumeration #1
+	reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v restrictanonymous /t REG_DWORD /d 1 /f 
+
+	rem Restrict Anonymous Enumeration #2
+	reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v restrictanonymoussam /t REG_DWORD /d 1 /f 
+
+	rem Idle Time Limit - 45 mins
+	reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v autodisconnect /t REG_DWORD /d 45 /f 
+
+	rem Require Security Signature - Disabled pursuant to checklist
+	reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v enablesecuritysignature /t REG_DWORD /d 0 /f 
+
+	rem Enable Security Signature - Disabled pursuant to checklist
+	reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v requiresecuritysignature /t REG_DWORD /d 0 /f 
+
+	rem Disable Domain Credential Storage
+	reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v disabledomaincreds /t REG_DWORD /d 1 /f 
+
+	rem Don't Give Anons Everyone Permissions
+	reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v everyoneincludesanonymous /t REG_DWORD /d 0 /f 
+
+	rem SMB Passwords unencrypted to third party
+	reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanWorkstation\Parameters /v EnablePlainTextPassword /t REG_DWORD /d 0 /f
+
+	rem Null Session Pipes Cleared
+	reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v NullSessionPipes /t REG_MULTI_SZ /d "" /f
+
+	rem remotely accessible registry paths cleared
+	reg ADD HKLM\SYSTEM\CurrentControlSet\Control\SecurePipeServers\winreg\AllowedExactPaths /v Machine /t REG_MULTI_SZ /d "" /f
+
+	rem remotely accessible registry paths and sub-paths cleared
+	reg ADD HKLM\SYSTEM\CurrentControlSet\Control\SecurePipeServers\winreg\AllowedPaths /v Machine /t REG_MULTI_SZ /d "" /f
+
+	rem Restict anonymous access to named pipes and shares
+	reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v NullSessionShares /t REG_MULTI_SZ /d "" /f
+
+	rem Allow to use Machine ID for NTLM
+	reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v UseMachineId /t REG_DWORD /d 0 /f
+
+	rem Enables DEP
+	bcdedit.exe /set {current} nx AlwaysOn
+	pause
+	goto :menu
+
+:audit
+	echo Auditing the maching now
+	auditpol /set /category:* /success:enable
+	auditpol /set /category:* /failure:enable
+
+	pause
+	goto :menu
+
+:group
+	cls
+	net localgroup
+	set /p grp=What group would you like to check?:
+	net localgroup !grp!
+	set /p answer=Is there a user you would like to add or remove?[add/remove/back]:
+	if "%answer%"=="add" (
+		set /p userAdd=Please enter the user you would like to add: 
+		net localgroup !grp! !userAdd! /add
+		echo !userAdd! has been added to !grp!
+	)
+	if "%answer%"=="remove" (
+		set /p userRem=Please enter the user you would like to remove:
+		net localgroup !grp! !userRem! /delete
+		echo !userRem! has been removed from !grp!
+	)
+	if "%answer%"=="back" (
+		goto :group
+	)
+
+	set /p answer=Would you like to go check again?[y/n]
+	if /I "%answer%"=="y" (
+		goto :group
+	)
+	if /I "%answer%"=="n" (
+		goto :menu
+	)
+endlocal
