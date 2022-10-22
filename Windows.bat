2bbfc33e49e4611519f8a6a9ddf00a66ee87c3dc
Wilsons77 <shaunp6907@gmail.com>
2022-10-22 00:49:37 -0400

    Update Windows.bat

--- Windows.bat
+++ Windows.bat
@@ -30,7 +30,7 @@ set /p answer=Have you answered all the forensics questions?[y/n]:
 	echo "5)Disable guest/admin	        14)Security options"
 	echo "6)Set password policy		15)Audit the machine"	
 	echo "7)Set lockout policy		16)Edit groups"
-	echo "8)Enable Firewall"
+	echo "8)Enable Firewall			17)Misc. commands"
 	echo "9)Search for media files"
 	echo "20)Quit			25)Restart"
 	echo "##############################################################"
@@ -51,6 +51,7 @@ set /p answer=Have you answered all the forensics questions?[y/n]:
 		if "%answer%"=="14" goto :secOpt
 		if "%answer%"=="15" goto :audit
 		if "%answer%"=="16" goto :group
+		if "%answer%"=="17 goto :misc
 		if "%answer%"=="20" exit
 		if "%answer%"=="25" shutdown /r
 	pause
@@ -164,29 +165,105 @@ set /p answer=Have you answered all the forensics questions?[y/n]:
 
 :badFiles
 	@echo off
-	set /p FILETYPE=What kind of file type do you need to find? Only put the file extension MP3,JPG,MOV:
-	:: the file to look for. (In this case 'myFile.!FILETYPE!')
-	set filename=*.!FILETYPE!
-
-	:: the drive or path to search. (In this case searching current drive)
-	set searchPath="C:\users"
-
-	:: If the file is found. This variable will be set
-	set foundFilePath=
-
-	:: echos all found paths and returns the last occurrance of the file path
-	FOR /R "%searchPath%" %%a  in (%filename%) DO (
-    	IF EXIST "%%~fa" (
-        echo "%%~fa" 
-        SET foundFilePath=%%~fa
-   			 )
-	)
-
-	IF EXIST "%foundFilePath%" (
- 	   echo The foundFilePath var is set to '%foundFilePath%'
-	) else (
-	    echo Could not find file '%filename%' under '%searchPath%'
-	)
+color 0f
+cls
+echo Flashing Disk to .flashed Files to reference....
+dir /b /s "C:\Program Files\" > programfiles.flashed
+dir /b /s "C:\Program Files (x86)\" >> programfiles.flashed
+echo Program Files flashed
+dir /b /s "C:\Users\" > users.flashed
+dir /b /s "C:\Documents and Settings" >> users.flashed
+echo User profiles flashed
+dir /b /s "C:\" > c.flashed
+echo C:\ Flashed
+pause
+echo Finding media files in C:\Users and/or C:\Documents and Settings...
+findstr .mp3 users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.mp3 > media_audio
+findstr .ac3 users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.ac3 >> media_audio
+findstr .aac users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.aac >> media_audio
+findstr .aiff users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.aiff >> media_audio
+findstr .flac users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.flac >> media_audio
+findstr .m4a users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.m4a >> media_audio
+findstr .m4p users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.m4p >> media_audio
+findstr .midi users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.midi >> media_audio
+findstr .mp2 users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.mp2 >> media_audio
+findstr .m3u users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.m3u >> media_audio
+findstr .ogg users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.ogg >> media_audio
+findstr .vqf users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.vqf >> media_audio
+findstr .wav users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.wav >> media_audio
+findstr .wma users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.wma >> media_video
+findstr .mp4 users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.mp4 >> media_video
+findstr .avi users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.avi >> media_video
+findstr .mpeg4 users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ .mpeg4 >> media_video
+REM BREAKLINE
+findstr .gif users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.gif >> media_pics
+findstr .png users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.png >> media_pics
+findstr .bmp users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.bmp >> media_pics
+findstr .jpg users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ .jpg >> media_pics
+findstr .jpeg users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ .jpeg >> media_pics
+C:\WINDOWS\system32\notepad.exe media_video
+C:\WINDOWS\system32\notepad.exe media_audio
+C:\WINDOWS\system32\notepad.exe media_pics
+echo Finding Hacktools now...
+findstr "Cain" programfiles.flashed
+if %errorlevel%==0 (
+echo Cain detected. Please take note, then press any key.
+pause >NUL
+)
+cls
+findstr "nmap" programfiles.flashed
+if %errorlevel%==0 (
+echo Nmap detected. Please take note, then press any key.
+pause >NUL
+)
+cls
+findstr "keylogger" programfiles.flashed
+if %errorlevel%==0 (
+echo Potential keylogger detected. Please take note, then press any key.
+pause >NUL
+)
+cls
+findstr "Armitage" programfiles.flashed
+if %errorlevel%==0 (
+echo Potential Armitage detected. Please take note, then press any key.
+pause >NUL
+)
+cls
+findstr "Metasploit" programfiles.flashed
+if %errorlevel%==0 (
+echo Potential Metasploit framework detected. Please take note, then press any key.
+pause >NUL
+)
+cls
+findstr "Shellter" programfiles.flashed
+if %errorlevel%==0 (
+echo Potential Shellter detected. Please take note, then press any key.
+pause >NUL
+)
+cls
+goto MENU
 	pause
 	goto :menu
 
@@ -230,7 +307,7 @@ set /p answer=Have you answered all the forensics questions?[y/n]:
 :UAC
 
 	rem Enable UAC
-	echo Turning on UAC has to be done manually.
+	reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 1 /f
 
 	pause
 	goto :menu
@@ -405,4 +482,127 @@ set /p answer=Have you answered all the forensics questions?[y/n]:
 	if /I "%answer%"=="n" (
 		goto :menu
 	)
+:misc
+	cls
+	REM Failsafe
+	if %errorlevel%==1 netsh advfirewall firewall set service type = remotedesktop mode = disable
+	echo Cleaning out the DNS cache...
+ipconfig /flushdns
+echo Writing over the hosts file...
+attrib -r -s C:\WINDOWS\system32\drivers\etc\hosts
+echo > C:\Windows\System32\drivers\etc\hosts
+if %errorlevel%==1 echo There was an error in writing to the hosts file (not running this as Admin probably)
+REM Services
+echo Showing you the services...
+net start
+echo Now writing services to a file and searching for vulnerable services...
+net start > servicesstarted.txt
+echo This is only common services, not nessecarily going to catch 100%
+REM looks to see if remote registry is on
+net start | findstr Remote Registry
+if %errorlevel%==0 (
+	echo Remote Registry is running!
+	echo Attempting to stop...
+	net stop RemoteRegistry
+	sc config RemoteRegistry start=disabled
+	if %errorlevel%==1 echo Stop failed... sorry...
+) else ( 
+	echo Remote Registry is already indicating stopped.
+)
+REM Remove all saved credentials
+cmdkey.exe /list > "%TEMP%\List.txt"
+findstr.exe Target "%TEMP%\List.txt" > "%TEMP%\tokensonly.txt"
+FOR /F "tokens=1,2 delims= " %%G IN (%TEMP%\tokensonly.txt) DO cmdkey.exe /delete:%%H
+del "%TEMP%\*.*" /s /f /q
+set SRVC_LIST=(RemoteAccess Telephony tlntsvr p2pimsvc simptcp fax msftpsvc)
+	for %%i in %HITHERE% do net stop %%i
+	for %%i in %HITHERE% sc config %%i start= disabled
+netsh advfirewall firewall set rule name="Remote Assistance (DCOM-In)" new enable=no >NUL
+netsh advfirewall firewall set rule name="Remote Assistance (PNRP-In)" new enable=no >NUL
+netsh advfirewall firewall set rule name="Remote Assistance (RA Server TCP-In)" new enable=no >NUL
+netsh advfirewall firewall set rule name="Remote Assistance (SSDP TCP-In)" new enable=no >NUL
+netsh advfirewall firewall set rule name="Remote Assistance (SSDP UDP-In)" new enable=no >NUL
+netsh advfirewall firewall set rule name="Remote Assistance (TCP-In)" new enable=no >NUL
+netsh advfirewall firewall set rule name="Telnet Server" new enable=no >NUL
+netsh advfirewall firewall set rule name="netcat" new enable=no >NUL
+dism /online /disable-feature /featurename:IIS-WebServerRole >NUL
+dism /online /disable-feature /featurename:IIS-WebServer >NUL
+dism /online /disable-feature /featurename:IIS-CommonHttpFeatures >NUL
+dism /online /disable-feature /featurename:IIS-HttpErrors >NUL
+dism /online /disable-feature /featurename:IIS-HttpRedirect >NUL
+dism /online /disable-feature /featurename:IIS-ApplicationDevelopment >NUL
+dism /online /disable-feature /featurename:IIS-NetFxExtensibility >NUL
+dism /online /disable-feature /featurename:IIS-NetFxExtensibility45 >NUL
+dism /online /disable-feature /featurename:IIS-HealthAndDiagnostics >NUL
+dism /online /disable-feature /featurename:IIS-HttpLogging >NUL
+dism /online /disable-feature /featurename:IIS-LoggingLibraries >NUL
+dism /online /disable-feature /featurename:IIS-RequestMonitor >NUL
+dism /online /disable-feature /featurename:IIS-HttpTracing >NUL
+dism /online /disable-feature /featurename:IIS-Security >NUL
+dism /online /disable-feature /featurename:IIS-URLAuthorization >NUL
+dism /online /disable-feature /featurename:IIS-RequestFiltering >NUL
+dism /online /disable-feature /featurename:IIS-IPSecurity >NUL
+dism /online /disable-feature /featurename:IIS-Performance >NUL
+dism /online /disable-feature /featurename:IIS-HttpCompressionDynamic >NUL
+dism /online /disable-feature /featurename:IIS-WebServerManagementTools >NUL
+dism /online /disable-feature /featurename:IIS-ManagementScriptingTools >NUL
+dism /online /disable-feature /featurename:IIS-IIS6ManagementCompatibility >NUL
+dism /online /disable-feature /featurename:IIS-Metabase >NUL
+dism /online /disable-feature /featurename:IIS-HostableWebCore >NUL
+dism /online /disable-feature /featurename:IIS-StaticContent >NUL
+dism /online /disable-feature /featurename:IIS-DefaultDocument >NUL
+dism /online /disable-feature /featurename:IIS-DirectoryBrowsing >NUL
+dism /online /disable-feature /featurename:IIS-WebDAV >NUL
+dism /online /disable-feature /featurename:IIS-WebSockets >NUL
+dism /online /disable-feature /featurename:IIS-ApplicationInit >NUL
+dism /online /disable-feature /featurename:IIS-ASPNET >NUL
+dism /online /disable-feature /featurename:IIS-ASPNET45 >NUL
+dism /online /disable-feature /featurename:IIS-ASP >NUL
+dism /online /disable-feature /featurename:IIS-CGI >NUL
+dism /online /disable-feature /featurename:IIS-ISAPIExtensions >NUL
+dism /online /disable-feature /featurename:IIS-ISAPIFilter >NUL
+dism /online /disable-feature /featurename:IIS-ServerSideIncludes >NUL
+dism /online /disable-feature /featurename:IIS-CustomLogging >NUL
+dism /online /disable-feature /featurename:IIS-BasicAuthentication >NUL
+dism /online /disable-feature /featurename:IIS-HttpCompressionStatic >NUL
+dism /online /disable-feature /featurename:IIS-ManagementConsole >NUL
+dism /online /disable-feature /featurename:IIS-ManagementService >NUL
+dism /online /disable-feature /featurename:IIS-WMICompatibility >NUL
+dism /online /disable-feature /featurename:IIS-LegacyScripts >NUL
+dism /online /disable-feature /featurename:IIS-LegacySnapIn >NUL
+dism /online /disable-feature /featurename:IIS-FTPServer >NUL
+dism /online /disable-feature /featurename:IIS-FTPSvc >NUL
+dism /online /disable-feature /featurename:IIS-FTPExtensibility >NUL
+dism /online /disable-feature /featurename:TFTP >NUL
+dism /online /disable-feature /featurename:TelnetClient >NUL
+dism /online /disable-feature /featurename:TelnetServer >NUL
+reg ADD "HKCU\Software\Microsoft\Internet Explorer\Main" /v DoNotTrack /t REG_DWORD /d 1 /f
+reg ADD "HKCU\Software\Microsoft\Internet Explorer\Download" /v RunInvalidSignatures /t REG_DWORD /d 1 /f
+reg ADD "HKCU\Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_LOCALMACHINE_LOCKDOWN\Settings" /v LOCALMACHINE_CD_UNLOCK /t REG_DWORD /d 1 /t
+reg ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v WarnonBadCertRecving /t REG_DWORD /d /1 /f
+reg ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v WarnOnPostRedirect /t REG_DWORD /d 1 /f
+reg ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v WarnonZoneCrossing /t REG_DWORD /d 1 /f
+reg ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v DisablePasswordCaching /t REG_DWORD /d 1 /f 
+reg ADD HKCU\SYSTEM\CurrentControlSet\Services\CDROM /v AutoRun /t REG_DWORD /d 1 /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\CrashControl /v CrashDumpEnabled /t REG_DWORD /d 0 /f
+
+REM Listing possible penetrations
+cd C:\
+echo "STARTING TO OUTPUT PROCESS FILES DIRECTLY TO THE C:\ DRIVE!"
+wmic process list brief > BriefProcesses.txt
+if %errorlevel%==1 echo Brief Processes failed to write
+wmic process list full >FullProcesses.txt
+if %errorlevel%==1 echo Full Processes failed to write
+wmic startup list full > StartupLists.txt
+if %errorlevel%==1 echo Startup Processes failed to write
+net start > StartedProcesses.txt
+if %errorlevel%==1 echo Started processes failed to write
+reg export HKLM\Software\Microsoft\Windows\CurrentVersion\Run  Run.reg
+if %errorlevel%==1 echo Run processes failed to write
+
+
+	pause
+	goto :menu
+	
+	
 endlocal
